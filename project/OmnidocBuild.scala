57a61cda44163516f19b476c8b8ee372ae80506f
James Roper <james@jazzy.id.au>
2015-09-07 11:38:18 +1000

    Upgrade to Play 2.4.3 and play-slick 1.0.1

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -12,10 +12,10 @@ object OmnidocBuild extends Build {
 
   val snapshotVersionLabel = "2.4.x"
 
-  val playVersion       = sys.props.getOrElse("play.version",       "2.4.2")
+  val playVersion       = sys.props.getOrElse("play.version",       "2.4.3")
   val anormVersion      = sys.props.getOrElse("anorm.version",      "2.4.0")
   val playEbeanVersion  = sys.props.getOrElse("play-ebean.version", "1.0.0")
-  val playSlickVersion  = sys.props.getOrElse("play-slick.version", "1.0.0")
+  val playSlickVersion  = sys.props.getOrElse("play-slick.version", "1.0.1")
   val maybeTwirlVersion = sys.props.get("twirl.version")
 
   // these dependencies pull in all the others
