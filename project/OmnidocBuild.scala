0fc1f2c4ccb97d1d8e4305401fe62bec1a666741
Play Team <play@vegemite.typesafe.com>
2016-10-08 16:00:55 -0700

    Update Play to 2.5.9

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -14,7 +14,7 @@ object OmnidocBuild extends Build {
 
   val snapshotVersionLabel = "2.5.x"
 
-  val playVersion              = sys.props.getOrElse("play.version",               "2.5.8")
+  val playVersion              = sys.props.getOrElse("play.version",               "2.5.9")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "1.5.1")
   val anormVersion             = sys.props.getOrElse("anorm.version",              "2.5.1")
   val playEbeanVersion         = sys.props.getOrElse("play-ebean.version",         "3.0.2")
