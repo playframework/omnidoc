305173c40eeaf9b93b24f25df5691a58fa3e0869
Play Team <play@vegemite.typesafe.com>
2017-05-22 17:11:22 -0700

    Play 2.6.0-RC1 and friends

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -14,12 +14,12 @@ object OmnidocBuild extends Build {
 
   val snapshotVersionLabel = "2.6.x"
 
-  val playVersion              = sys.props.getOrElse("play.version",               "2.6.0-M5")
-  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "3.0.0-M2")
+  val playVersion              = sys.props.getOrElse("play.version",               "2.6.0-RC1")
+  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "3.0.0-RC1")
   val anormVersion             = sys.props.getOrElse("anorm.version",              "2.5.3")
-  val playEbeanVersion         = sys.props.getOrElse("play-ebean.version",         "4.0.0-M3")
-  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.6.0-M7")
-  val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "3.0.0-M5")
+  val playEbeanVersion         = sys.props.getOrElse("play-ebean.version",         "4.0.0-RC1")
+  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.6.0-RC1")
+  val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "3.0.0-RC1")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
 
   // these dependencies pull in all the others
