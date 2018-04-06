79ba3099b9c2eb2bbf91a55bcb95fd6e66dcb41d
Play Team <play@vegemite.typesafe.com>
2018-04-06 12:22:13 -0700

    Play 2.6.13

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -14,7 +14,7 @@ object OmnidocBuild extends Build {
 
   val snapshotVersionLabel = "2.6.x"
 
-  val playVersion              = sys.props.getOrElse("play.version",               "2.6.12")
+  val playVersion              = sys.props.getOrElse("play.version",               "2.6.13")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "3.1.2")
   val anormVersion             = sys.props.getOrElse("anorm.version",              "2.5.3")
   val playEbeanVersion         = sys.props.getOrElse("play-ebean.version",         "4.1.0")
