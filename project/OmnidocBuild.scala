b583e567b94e0a78196b9022b53416bda1b2504b
Marcos Pereira <marcos.silva@gmail.com>
2019-04-18 19:28:53 -0400

    Play 2.7.1

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -16,8 +16,8 @@ object OmnidocBuild {
 
   val snapshotVersionLabel = "2.7.x"
 
-  val playVersion              = sys.props.getOrElse("play.version",               "2.7.0")
-  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "4.0.1")
+  val playVersion              = sys.props.getOrElse("play.version",               "2.7.1")
+  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "4.0.2")
   val playEbeanVersion         = sys.props.getOrElse("play-ebean.version",         "5.0.1")
   val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.7.2")
   val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "4.0.0")
