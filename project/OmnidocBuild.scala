664baf05ef18ff23f76b71819141037a4c57eab6
Renato Cavalcanti <renato@cavalcanti.be>
2019-06-20 11:34:51 +0200

    update to Play v2.7.3 and other projects
    * play-slick v4.0.2
    * scalatestplus-play v4.0.3

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -17,10 +17,10 @@ object OmnidocBuild {
   val snapshotVersionLabel = "2.7.x"
 
   val playVersion              = sys.props.getOrElse("play.version",               "2.7.3")
-  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "4.0.2")
+  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "4.0.3")
   val playEbeanVersion         = sys.props.getOrElse("play-ebean.version",         "5.0.1")
   val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.7.4")
-  val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "4.0.1")
+  val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "4.0.2")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
 
   // List Play artifacts so that they can be added as dependencies
