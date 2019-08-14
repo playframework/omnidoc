26de30577364044fe953f840fd3f9258538a1450
Scala Steward <me@scala-steward.org>
2019-08-14 03:20:32 +0200

    Update play-functional, play-json to 2.8.0-M5

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -18,7 +18,7 @@ object OmnidocBuild {
 
   val playVersion              = sys.props.getOrElse("play.version",               "2.8.0-M3")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "5.0.0-M3")
-  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.8.0-M4")
+  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.8.0-M5")
   val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "5.0.0-M4")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
 
