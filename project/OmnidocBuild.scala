5a9312dcc261c32b2044aee4db8d067869d43b77
Scala Steward <me@scala-steward.org>
2019-10-30 22:45:33 +0100

    Update play-slick, play-slick-evolutions to 5.0.0-RC1

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -19,7 +19,7 @@ object OmnidocBuild {
   val playVersion              = sys.props.getOrElse("play.version",               "2.8.0-M6")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "5.0.0-M6")
   val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.8.0-M6")
-  val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "5.0.0-M7")
+  val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "5.0.0-RC1")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
 
   // List Play artifacts so that they can be added as dependencies
