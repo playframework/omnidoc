99aa766920d0be78cb04a8379a2dd961307f0370
Scala Steward <me@scala-steward.org>
2020-12-14 15:27:09 +0100

    Update play, play-ahc-ws, ... to 2.8.7

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -20,7 +20,7 @@ object OmnidocBuild {
 
   val snapshotVersionLabel = "2.8.x"
 
-  val playVersion              = sys.props.getOrElse("play.version",               "2.8.5")
+  val playVersion              = sys.props.getOrElse("play.version",               "2.8.7")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "5.1.0")
   val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.9.1")
   val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "5.0.0")
