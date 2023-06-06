970ca9e27357b314cf434d2a8cfe1652601d87c9
Scala Steward <scala_steward@virtuslab.com>
2023-06-06 20:30:12 +0000

    Update scalatestplus-play to 6.0.0-M6

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -21,7 +21,7 @@ object OmnidocBuild {
   val snapshotVersionLabel = "2.9.x"
 
   val playVersion              = sys.props.getOrElse("play.version",               "2.9.0-M5")
-  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "6.0.0-M4")
+  val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "6.0.0-M6")
   val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.10.0-RC9")
   val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "5.1.0")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
