ee3468f3f83ca21567d00438f840954393f92b6e
Scala Steward <scala_steward@virtuslab.com>
2023-10-26 22:07:10 +0000

    play-functional, play-json 3.0.0 (was 2.10.2)

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -20,7 +20,7 @@ object OmnidocBuild {
 
   val playVersion              = sys.props.getOrElse("play.version",               "2.9.0")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "6.0.0")
-  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "2.10.2")
+  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "3.0.0")
   val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "5.2.0-RC1")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
 
