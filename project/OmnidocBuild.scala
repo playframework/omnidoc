769e9e86d24d119e2100843e53a540e266598491
Matthias Kurz <m.kurz@irregular.at>
2024-03-01 16:19:19 +0100

    play-json 3.0.2

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -21,7 +21,7 @@ object OmnidocBuild {
 
   val playVersion              = sys.props.getOrElse("play.version",               "3.0.1")
   val scalaTestPlusPlayVersion = sys.props.getOrElse("scalatestplus-play.version", "7.0.1")
-  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "3.0.1")
+  val playJsonVersion          = sys.props.getOrElse("play-json.version",          "3.0.2")
   val playSlickVersion         = sys.props.getOrElse("play-slick.version",         "6.0.0-M2")
   val maybeTwirlVersion        = sys.props.get("twirl.version")
 
