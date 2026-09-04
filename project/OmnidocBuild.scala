363d38b0f2e6cccce3d4fbce657f1aa563c5bc98
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 22:37:57 +0000

    [3.0.x] scala3-library 3.9.0 (was 3.3.8)

--- project/OmnidocBuild.scala
+++ project/OmnidocBuild.scala
@@ -10,7 +10,7 @@ import java.io.IOException
 object OmnidocBuild {
 
   val scala213 = "2.13.18"
-  val scala3 = "3.3.8"
+  val scala3 = "3.9.0"
 
   val playOrganisation = "org.playframework"
   val twirlOrganisation = "org.playframework.twirl"
